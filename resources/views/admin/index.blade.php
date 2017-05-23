f10976352d9038e97edfeddc60ba6b0f0b18f9d8
Krating <c.kullawat@gmail.com>
2017-05-23 15:09:41 +0700

    commit

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,8 +1,23 @@
 @extends('layouts.master')
 @section('content')
 
-	<a class="btn btn-info" href="{{ route('product.index') }}">Stock</a>
-	<a class="btn btn-info" href="{{ route('booking.index') }}">Booking</a>
-	<a class="btn btn-info" href="{{ route('payment.index') }}">Payment</a>
+<div class="container">
+	<div class="row">
+		<div class="col-md-3 col-md-offset-1 box">
+			<div class="login-container smbox">${{ $totals }}<br>Total bookings made today</div>
+		</div>
+		<div class="col-md-3 box">
+			<div class="login-container smbox">Today's Booking</div>
+		</div>
+		<div class="col-md-3 box">
+			<div class="login-container smbox">?</div>
+		</div>
+	</div>
+	<div class="row box">
+		<div class="col-md-9 col-md-offset-1">
+			<div class="login-container bigbox">Graph</div>
+		</div>
+	</div>
+</div>
 
 @endsection
