b1f1fcccd4f023b8858c2cee2888e4ff92622ed5
Krating <c.kullawat@gmail.com>
2017-07-14 14:51:54 +0700

    Procfile for Heroku

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_96df5c019542c5a'),
+            'username' => env('DB_USERNAME', 'bcff45d537cf03'),
+            'password' => env('DB_PASSWORD', '78516978'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
